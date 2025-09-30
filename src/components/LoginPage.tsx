cebe10bb2c4b7c3c6be8cc070dcc6ab5e6664d77
Kanna Perumal <kannangates@gmail.com>
2025-09-30 22:10:54 +0530

    import fix n card title and descriptin

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -9,9 +9,9 @@ import { Input } from '@/components/ui/input';
 import {
   Card,
   CardContent,
-  CardDescription,
   CardHeader,
-  CardTitle,
+  // CardDescription,
+  // CardTitle,
 } from '@/components/ui/card';
 import {
   Form,
